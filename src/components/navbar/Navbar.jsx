3e2f4a9ea9af104aa826e0dbf67cb5307779f843
Jawid Fakhari <jawid_fakhari@live.it>
2024-03-02 12:23:12 +0100

    how to dinamyc value in react

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import styled from './navbar.module.css';
 
 function Navbar() {
+    let myLogo = '🐼';
+
   return (
     <div className={styled.navbar}>
+        <div className={styled.logo}>{myLogo}</div>
       <ul>
         <li>Portfolio</li>
         <li>New Project</li>
