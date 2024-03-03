e54fb9d80b84bfa6a809c9b96f6832a663550e3a
Jawid Fakhari <jawid_fakhari@live.it>
2024-03-03 20:28:41 +0100

    map data locally and render data

--- src/components/projectBox/ProjectBox.jsx
+++ src/components/projectBox/ProjectBox.jsx
@@ -1,10 +1,13 @@
 import styled from './projectBox.module.css';
-import projectImage from '../../assets/images/tclasher.png'
+import projectImage from '../../assets/images/tclasher.png';
+// import Home from '../../pages/home/Home';
 
 function ProjectBox(props) {
+
     return (
         <div className={styled.projectBoxWrapper}>
             <img src={projectImage} alt="" />
+            <div className={styled.titleSpace}><span>{props.data.title}</span><span className={styled.arrBtn}>&#128437;</span></div>
         </div>
     );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,19 +2,38 @@ import Navbar from "../../components/navbar/Navbar";
 import ProjectBox from "../../components/projectBox/ProjectBox";
 import styled from "./home.module.css";
 
+
 function Home(props) {
+    let info = [
+        {
+            "id": 1,
+            "imgUrl": "",
+            "title": "T Clasher",
+            "visitSrcUrl": ""
+        },
+        {
+            "id": 2,
+            "imgUrl": "",
+            "title": "Crowd Funding",
+            "visitSrcUrl": ""
+        },
+        {
+            "id": 3,
+            "imgUrl": "",
+            "title": "Interaction Pricing",
+            "visitSrcUrl": ""
+        }
+    ]
     return (
         <div className={styled.homeWrapper}>
             <Navbar title="navbar" logoSrc="🐼" />
             <div className="container">
                 <h2>My Works</h2>
                 <div className={styled.projectsWrapper}>
-                    <ProjectBox />
-                    <ProjectBox />
-                    <ProjectBox />
-                    <ProjectBox />
-                    <ProjectBox />
-                    <ProjectBox />
+                    {info.map(data => (
+                        < ProjectBox data={data} />
+                    ))
+                    }
                 </div>
             </div>
         </div>
