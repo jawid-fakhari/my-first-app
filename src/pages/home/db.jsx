ea199e0644921cec41e47fbf8ae75f96f3cc2df2
Jawid Fakhari <jawid_fakhari@live.it>
2024-03-06 21:39:36 +0100

    more projects added

--- src/pages/home/db.jsx
+++ src/pages/home/db.jsx
@@ -1,32 +1,104 @@
 const projects = [
     {
+        id: 1,
         name: "Tclasher",
         image: "/images/tclasher.png",
         link: "#"
     },
     {
+        id: 2,
         name: "Crowd Funding",
         image: "./images/crowdFunding.png",
         link: "#"
     },
     {
+        id: 3,
         name: "Bill Spliter",
         image: "/images/spliter.png",
         link: "#"
     },
     {
+        id: 4,
         name: "Weather App",
         image: "/images/weather.png",
         link: "#"
     },
     {
+        id: 5,
         name: "To Do List",
         image: "/images/toDoList.png",
         link: "#"
     },
     {
+        id: 6,
         name: "Simpple Quiz",
         image: "/images/simpleQuiz.png",
+        link: "#"
+    },
+    {
+        id: 7,
+        name: "Password Maker App",
+        image: "./images/passwordMaker.png",
+        link: "#"
+    },
+    {
+        id: 8,
+        name: "CSS",
+        image: "./images/CSSWork.png",
+        link: "#"
+    },
+    {
+        id: 9,
+        name: "CSS",
+        image: "/images/CSSWork1.png",
+        link: "#"
+    },
+    {
+        id: 10,
+        name: "CSS",
+        image: "/images/CSSWork2.png",
+        link: "#"
+    },
+    {
+        id: 11,
+        name: "CSS grid",
+        image: "/images/gridCSS.png",
+        link: "#"
+    },
+    {
+        id: 12,
+        name: "CSS grid",
+        image: "/images/gridCSS1.png",
+        link: "#"
+    },
+    {
+        id: 13,
+        name: "CSS grid",
+        image: "/images/gridCSS2.png",
+        link: "#"
+    },
+    {
+        id: 14,
+        name: "Account Maker",
+        image: "/images/accountMaker.png",
+        link: "#"
+    },
+    {
+        id: 15,
+        name: "Trafic Based Price",
+        image: "/images/traficBasedPrice.png",
+        link: "#"
+    },
+    {
+        id: 16,
+        name: "UX",
+        image: "/images/uxWork.png",
+        link: "#"
+    },
+    {
+        id: 17,
+        name: "UX",
+        image: "/images/uxWork1.png",
         link: "#"
     },
 ];
