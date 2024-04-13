570739253fbc9bbd38754d1bf8d96de84d5c523a
Jawid Fakhari <jawid_fakhari@live.it>
2024-04-13 16:37:50 +0200

    contact email.js connected and it works.

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
+import React, { useRef } from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import emailjs from '@emailjs/browser';
+import styled from './contac.module.css';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_xyzl5hl', 'template_tgem0uk', form.current, {
+                publicKey: 'JPqlwpnYCcfSkshgJ',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    alert('Your message successfully sended!')
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+
+    };
     return (
         <div>
             <Navbar />
+            <form className={styled.form} ref={form} onSubmit={sendEmail}>
+                <div className={styled.nameEmailWrap}>
+                    <div className={styled.nameWrap}>
+                        <label className={styled.nameLabel}>Name</label>
+                        <input className={styled.nameInput} type="text" name="user_name" />
+                    </div>
+                    <div className={styled.emailWrap}>
+                        <label className={styled.emailLabel}>Email</label>
+                        <input className={styled.emailInput} type="email" name="user_email" />
+                    </div>
+                </div>
+
+                <label className={styled.messageLabel}>Message</label>
+                <textarea className={styled.messageTextarea} name="message" />
+
+                <input className={styled.submitInput} type="submit" value="Send" />
+            </form>
         </div>
     )
 }
