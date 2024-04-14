ece168217358195582d5d197f60a8a435a23a44a
Jawid Fakhari <jawid_fakhari@live.it>
2024-04-14 12:36:46 +0200

    contact page completed

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,6 +2,8 @@ import React, { useRef } from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import emailjs from '@emailjs/browser';
 import styled from './contac.module.css';
+import { FaChevronCircleRight, FaGithub, FaLinkedin } from 'react-icons/fa';
+
 
 const Contact = () => {
     const form = useRef();
@@ -28,6 +30,22 @@ const Contact = () => {
     return (
         <div>
             <Navbar />
+
+            <div className={styled.linkUtili}>
+                <div className={`${styled.cv} ${styled.commonStyle}`}>
+                    <FaChevronCircleRight size={35} color='#301934' />
+                    <a href="https://europa.eu/europass/eportfolio/screen/share/4626b2ae-978e-4825-a687-1b12301a68b9?lang=en" target='_blank' rel="noreferrer">Europass CV</a>
+                </div>
+                <div className={`${styled.github} ${styled.commonStyle}`}>
+                    <FaGithub size={35} color='#301934' />
+                    <a href="https://github.com/jawid-fakhari" target='_blank' rel="noreferrer">My Github profile</a>
+                </div>
+                <div className={`${styled.linkedin} ${styled.commonStyle}`}>
+                    <FaLinkedin size={35} color='#301934' />
+                    <a href="https://www.linkedin.com/in/fakhari-jawid" target='_blank' rel="noreferrer">My Linkedin profile</a>
+                </div>
+            </div>
+
             <form className={styled.form} ref={form} onSubmit={sendEmail}>
                 <div className={styled.nameEmailWrap}>
                     <div className={styled.nameWrap}>
@@ -46,6 +64,7 @@ const Contact = () => {
                 <input className={styled.submitInput} type="submit" value="Send" />
             </form>
         </div>
+
     )
 }
 
